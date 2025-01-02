f432f81ff3a67e932044d1e7364cbd49e750435e
zakariaeelhassad <zakariaelhassad031@gmail.com>
2025-01-02 12:19:42 +0100

    modifer dans le page member et projectmanager

--- src/model/Member.php
+++ src/model/Member.php
@@ -1,9 +1,11 @@
 <?php
 
+require_once "User.php";
 class Member extends User {
     private array $task;
 
-    public function __construct(array $task){
+    public function __construct(int $id, string $name, string $email, string $password , array $task){
+        parent::__construct( $id,  $name,  $email,  $password);
         $this->task = $task;
     }
 
--- src/model/ProjectManager.php
+++ src/model/ProjectManager.php
@@ -1,9 +1,12 @@
 <?php
-
+require_once "User.php";
 class ProjectManager extends User {
     private array $project;
 
-    public function __construct(array $project){
+    public function __construct(int $id, string $name, string $email, string $password , array $project)
+    {
+
+        parent::__construct( $id,  $name,  $email,  $password);
         $this->project = $project;
     }
 
